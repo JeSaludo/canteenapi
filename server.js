78daefec8308f68741849b1416f657ac3b79f550
Jan Eris Saludo <janerissaludo@gmail.com>
2024-11-09 03:13:33 +0800

    init

---
+++ server.js
@@ -0,0 +1,26 @@
+// server.js
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 5000;  // Default to 5000 if not set
+const cors = require('cors');
+
+app.use(cors());
+// Middleware
+
+app.use(express.json()); // For parsing application/json
+
+  // Enable CORS
+// Example route
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'Hello from Node.js!', status: 'success' });
+});
